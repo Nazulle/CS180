f0272aee91b1a2190c158bd8ff7e744ff3832ef0
jmw6055 <60833290+jmw6055@users.noreply.github.com>
2021-04-22 03:51:33 -0400

    4.22 minoj

--- Profile/Authentication.java
+++ Profile/Authentication.java
@@ -1,13 +1,68 @@
+import java.util.ArrayList;
+
+/**
+ * Authentication Class will be the ArrayList of Profiles in the server.
+ *  @author Minwoo Jung - jung361@purdue.edu, Saketh Ayyalasomayajula - sayyala@purdue.edu
+ *  @version April 22th
+ */
+
 public class Authentication {
-    private ArrayList <Profile> profiles;
-    
-    public Authentication() {
-        profiles = new ArrayList<Profile>();
+    ArrayList<Profile> profiles = new ArrayList<Profile>();
+
+    public Authentication(ArrayList<Profile> profiles) {
+        this.profiles = profiles;
     }
 
+    /**
+     * Create a new Profile with username & password. If the profile exist in the list, return null.
+     * @return new Profile or null
+     */
     public Profile createProfile(String username, String password) {
         Profile prof = new Profile(username, password);
-        profiles.add(prof);
+        boolean isProfileExists = false;
+        for (Profile p : profiles) {
+            if (p.getUsername().equals(username)) {
+                isProfileExists = true;
+                break;
+            }
+        }
+        if (isProfileExists)
+            return null;
+        else {
+            profiles.add(prof);
+            return prof;
+        }
     }
 
+    /**
+     * Find if there is matching profile with username & password. If not, return false.
+     * @return profile p from the list
+     */
+    public Profile login(String username, String password) {
+        for (Profile p : profiles) {
+            if (p.getUsername().equals(username) && p.getPassword().equals(password)) {
+                return p;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Get a profile from the List
+     * @return the profile
+     */
+    public Profile getProfile(String username) {
+        for (Profile p : profiles) {
+            if (p.getUsername().equals(username)) {
+                return p;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        return profiles.toString();
+    }
 }
+
