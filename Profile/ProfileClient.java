0b411da2366eb5e3757e4210ab064d1f8bf512b9
jmw6055 <60833290+jmw6055@users.noreply.github.com>
2021-04-26 15:38:31 -0400

    Update ProfileClient.java

--- Profile/ProfileClient.java
+++ Profile/ProfileClient.java
@@ -1,6 +1,4 @@
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+
 import java.io.*;
 import java.net.*;
 import javax.swing.*;
@@ -10,7 +8,7 @@ import javax.swing.*;
  *
  * This class consists of a client that handles user inputs for basic equations through displaying GUIs.
  *	
- * @author Saketh Ayyalasomayajula, sayyala@purdue.edu
+ * @author Saketh Ayyalasomayajula(sayyala@purdue.edu), Minwoo Jung(jung361@purdue.edu)
  * @version April 19th, 2021
  */
 
@@ -24,16 +22,18 @@ public class ProfileClient {
 
         JOptionPane.showMessageDialog(null, "Welcome to the Social Media Profile App!", "Profile App", JOptionPane.INFORMATION_MESSAGE);
         try {
-            socket = new Socket("localhost", 4242);
+            socket = new Socket("10.0.0.234", 4242);
 
             //login code
-            LoginGUI g = new LoginGUI();
+            LoginGUI loginGUI = new LoginGUI();
+            GUIMain guiMain = new GUIMain();
             SwingUtilities.invokeLater(new Runnable() {
                 public void run() {
-                    g.run();
+                    loginGUI.run();
 
                 }
             });
+
 
 
             // create/ edit account
@@ -49,7 +49,7 @@ public class ProfileClient {
         }
     }
 
-    public static void sendLoginInfo() throws IOException {
+    public static boolean sendLoginInfo(Socket socket) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         PrintWriter writer = new PrintWriter(socket.getOutputStream());
         if (socket.isConnected()) {
@@ -59,13 +59,22 @@ public class ProfileClient {
             writer.println(password);
             writer.flush();
             String result = reader.readLine();
-            JOptionPane.showMessageDialog(null, result, "Profile App", JOptionPane.INFORMATION_MESSAGE);
+            if (result.equals("success")) {
+                JOptionPane.showMessageDialog(null, "Welcome!", "Profile App", JOptionPane.INFORMATION_MESSAGE);
+                return true;
+            }
+            else {
+                JOptionPane.showMessageDialog(null, "No matching username or password.", "Profile App", JOptionPane.INFORMATION_MESSAGE);
+                return false;
+            }
         }
-        else
+        else {
             JOptionPane.showMessageDialog(null, "Not connected", "Profile App", JOptionPane.INFORMATION_MESSAGE);
+            return false;
+        }
     }
 
-    public static void createAccount() throws IOException {
+    public static void createAccount(Socket socket) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         PrintWriter writer = new PrintWriter(socket.getOutputStream());
         if (socket.isConnected()) {
